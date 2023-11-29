8493a6994bbe5a9777e780c217c513dd377e1dda
bosombaby <1245924849@qq.com>
2023-11-29 11:39:13 +0800

    feat✨: base done

--- this指向/03_bind指向.js
+++ this指向/03_bind指向.js
@@ -1,18 +1,22 @@
 const person = {
-    name: 'Alice',
-    age: 20
+  name: "Alice",
+  age: 20,
+};
+const person1 = {
+  name: "Alice1",
+  age: 20,
 };
 
 function sayHello() {
-    console.log(`Hello, my name is ${this.name}, I'm ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name}, I'm ${this.age} years old.`);
 }
 
-const sayHello2 = sayHello.bind(person);
-sayHello2(); // 输出 "Hello, my name is Alice, I'm 20 years old."
-
+const sayHello2 = sayHello.bind(person1).bind(person);
+// sayHello2(); // 输出 "Hello, my name is Alice, I'm 20 years old."
+sayHello();
 
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 const add5 = add.bind(null, 5);
--- this指向/04_apply和call.js
+++ this指向/04_apply和call.js
@@ -1,19 +1,21 @@
-const name = '小王', age = 17
+const name = "小王",
+  age = 17;
 
 const obj = {
-    name: '小张',
-    objAge: this.age,
-    myFun: function (fm, t) {
-        console.log(this.name + '年龄' + this.age)
-        console.log(fm + t)
-    }
-}
+  name: "小张",
+  objAge: this.age,
+  myFun: function (fm, t) {
+    console.log(this.name + "年龄" + this.age);
+    console.log(fm + t);
+  },
+};
 
 const db = {
-    name: '小李',
-    age: 18
-}
+  name: "小李",
+  age: 18,
+};
 
-obj.myFun.bind(db, '北京', '上海')()
-obj.myFun.call(db, '北京', '上海')
-obj.myFun.apply(db, ['北京', '上海'])
+// obj.myFun.bind(db, "北京", "上海")();
+obj.myFun.call(db, "北京", "上海");
+obj.myFun.call(db, "北京", "上海");
+// obj.myFun.apply(db, ['北京', '上海'])
