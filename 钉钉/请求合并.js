52b4f1dbb9b4b228c0ce0e7100dfe852fd5c78c8
bosom <1245924849@qq.com>
2023-12-03 23:56:43 +0800

    feat✨: 请求合并

--- 钉钉/请求合并.js
+++ 钉钉/请求合并.js
@@ -1,24 +1,28 @@
 let taskQueue = []
 let timer = null
 function sendTasks() {
-  console.log(taskQueue)
+  if (taskQueue.length) console.log(taskQueue)
   taskQueue = []
 }
 
-export function debounce(task) {
+export function debounce(task, callNow = false) {
   taskQueue.push(task)
-  if (timer) clearTimeout(timer)
-  timer = setTimeout(() => {
-    sendTasks()
-  }, 100)
+
+  if (callNow) sendTasks()
+  else {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      sendTasks()
+    }, 100)
+  }
 }
 
 for (let i = 0; i < 10; i++) {
-  debounce(i)
+  debounce(i, true)
 }
 
 setTimeout(() => {
   for (let i = 10; i < 100; i++) {
     debounce(i)
   }
-}, 1000)
+}, 100)
