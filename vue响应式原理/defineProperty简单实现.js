7a56ec15347640766628b02c954a3beab6cd1df8
bosombaby <1245924849@qq.com>
2023-12-04 17:15:26 +0800

    响应式原理

--- vue响应式原理/defineProperty简单实现.js
+++ vue响应式原理/defineProperty简单实现.js
@@ -2,15 +2,15 @@ let obj = {}
 Object.defineProperty(obj, 'name', {
   enumerable: true,
   configurable: true,
+
   get: function () {
-    console.log('访问数据，结果为：', this._name)
+    console.log('访问数据', this._name)
   },
   set: function (newValue) {
-    if (newValue === this._name) return
-
     this._name = newValue
-    console.log('修改数据，结果为', this._name)
+
+    console.log('修改数据', this._name)
   },
 })
-obj.name = 11
-obj.name
+
+obj.name = 111
