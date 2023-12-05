8bbdaf78641eb8af191cb94733308eed1ad84299
bosombaby <1245924849@qq.com>
2023-12-05 14:07:04 +0800

    feat✨: 模版匹配

---
+++ 场景题/模版渲染.js
@@ -0,0 +1,19 @@
+function parseTemplateString(templateString, data) {
+  const regex = /\${(.*?)}/g
+  const str = templateString.replace(regex, (match, key) => {
+    return eval(`data.${key}`)
+  })
+
+  return str
+}
+
+const templateString =
+  '我的名字是 ${name}，我今年 ${age} 岁，我从事 ${job} 的工作。'
+const person = {
+  name: 'zs',
+  age: 18,
+  job: '程序员',
+}
+
+const result = parseTemplateString(templateString, person)
+console.log(result)
