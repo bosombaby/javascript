ea6eef868f14684d10f7bb5e3307e23b31b0e4d4
bosom <1245924849@qq.com>
2024-01-15 22:58:39 +0800

    feat✨: 算法题

--- 面试/05_两数之和.js
+++ 面试/05_两数之和.js
@@ -1,10 +1,13 @@
-var twoSum = function (nums, target) {
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+
+function twoSum(nums, target) {
   let map = new Map(),
     count
   for (let i = 0; i < nums.length; i++) {
     count = target - nums[i]
     if (map.has(count)) return [map.get(count), i]
-    else map.set(nums[i], i)
+    map.set(nums[i], i)
   }
   return []
 }
