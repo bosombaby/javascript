6871174ce26cedf1e7ab2cc22f82962541a8d68a
bosom <1245924849@qq.com>
2024-02-28 11:39:59 +0800

    feat: 懒加载教程

--- 防抖与节流/01_防抖立即执行.js
+++ 防抖与节流/01_防抖立即执行.js
@@ -1,34 +1,43 @@
 const inputDom = document.querySelector('#input-dom')
+const btnDom = document.querySelector('#click-btn')
 
 //防抖
 function debounce(fn, delay, immediate) {
-    let timer = null
-    return function () {
-        let context = this
-        let args = arguments
+  let timer = null
+  return function () {
+    let context = this
+    let args = arguments
 
-        if (timer)
-            clearTimeout(timer)
+    if (timer) clearTimeout(timer)
 
-        if (immediate) {
-            let callNow = !timer
-            timer = setTimeout(() => {
-                timer = null
-            }, delay)
+    if (immediate) {
+      let callNow = !timer
+      timer = setTimeout(() => {
+        timer = null
+      }, delay)
 
-            if (callNow)
-                fn.apply(context, args)
-
-        } else {
-            timer = setTimeout(() => {
-                fn.apply(context, args)
-            }, delay)
-        }
-
+      if (callNow) fn.apply(context, args)
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(context, args)
+      }, delay)
     }
+  }
 }
 
+inputDom.addEventListener(
+  'input',
+  debounce(
+    function () {
+      console.log('发送HTTP请求')
+    },
+    500,
+    true
+  )
+)
 
-inputDom.addEventListener('input', debounce(function () {
-    console.log('发送HTTP请求');
-}, 500, true))
+btnDom.addEventListener('click', debounce(searchMsg, 500))
+
+function searchMsg() {
+  console.log('搜索数据')
+}
