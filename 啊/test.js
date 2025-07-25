6c197318cfccdaad867352195a50afddcbc91376
riskbaby <1245924849@qq.com>
2025-07-25 17:44:29 +0800

    feat: reduce 原生方法

--- 啊/test.js
+++ 啊/test.js
@@ -1,22 +1,12 @@
-// 名称：对象扁平化
-// 描述：
-//  - 将嵌套对象扁平化为一层
-//  - 输入：
-// const obj = {
-//   a: 1,
-//   b: [1, 2, { c: true }],
-//   c: { e: 2, f: 3 },
-//   g: null,
-// }
-//  - 输出：
-/* {
-   a: 1,
-   'b[0]': 1,
-   'b[1]': 2,
-   'b[2].c': true,
-   'c.e': 2,
-   'c.f': 3,
-   g: null
+function reduceFlatArray(target, depth) {
+  return target.reduce((acc, cur) => {
+    if (Array.isArray(cur) && depth) acc.push(...reduceFlatArray(cur, depth - 1))
+    else acc.push(cur)
+
+    return acc
+  }, [])
 }
-*/
-// 考察: 递归、对象操作
+
+const array = [1, [2, 3], [4, [5, 6, [7, 8]]]]
+
+console.log(reduceFlatArray(array, 1))
--- 啊/数组扁平化.js
+++ 啊/数组扁平化.js
@@ -4,34 +4,34 @@
 //  - 输入： [1, [2, 3], [4, [5, 6, [7, 8]]]]
 // 考察: 数组操作、递归解析、reduce
 
-console.log("\n --- 一：原生方法 --- \n");
+console.log('\n --- 一：原生方法 --- \n')
 function originFlat(target, depth) {
-  return target.flat(depth);
+  return target.flat(depth)
 }
 
-console.log(originFlat([1, [2, 3], [4, [5, 6, [7, 8]]]], 5));
+console.log(originFlat([1, [2, 3], [4, [5, 6, [7, 8]]]], 5))
 
-console.log("\n --- 二：reduce 叠加 --- \n");
+console.log('\n --- 二：reduce 叠加 --- \n')
 function reduceFlat(target, depth) {
   return target.reduce((acc, cur) => {
-    if (Array.isArray(cur) && depth)
-      return acc.concat(reduceFlat(cur, depth - 1));
-    return acc.concat(cur);
-  }, []);
+    if (Array.isArray(cur) && depth) acc.push(...reduceFlat(cur, depth - 1))
+    else acc.push(cur)
+
+    return acc
+  }, [])
 }
 
-console.log(reduceFlat([1, [2, 3], [4, [5, 6, [7, 8]]]], 10));
+console.log(reduceFlat([1, [2, 3], [4, [5, 6, [7, 8]]]], 10))
 
-console.log("\n --- 三：递归实现 --- \n");
+console.log('\n --- 三：递归实现 --- \n')
 function recFlat(target, depth) {
-  let result = [];
-  target.forEach((item) => {
-    if (Array.isArray(item) && depth)
-      result = result.concat(recFlat(item, depth - 1));
-    else result.push(item);
-  });
+  let result = []
+  target.forEach(item => {
+    if (Array.isArray(item) && depth) result = result.concat(recFlat(item, depth - 1))
+    else result.push(item)
+  })
 
-  return result;
+  return result
 }
 
-console.log(recFlat([1, [2, 3], [4, [5, 6, [7, 8]]]], 10));
+console.log(recFlat([1, [2, 3], [4, [5, 6, [7, 8]]]], 10))
